1806675cfeee6ad0d62c561b67e004e2e79c8e1e
rafiquekarnool-cmd <rafiquekarnool@gmail.com>
2026-09-21 10:21:12 +0530

    Add Electron desktop launcher

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 900,
+    minWidth: 1024,
+    minHeight: 700,
+    show: false,
+    backgroundColor: '#0b2f5b',
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  win.loadFile('index.html');
+  win.once('ready-to-show', () => { win.maximize(); win.show(); });
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (/^https?:/i.test(url)) shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
+});
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
